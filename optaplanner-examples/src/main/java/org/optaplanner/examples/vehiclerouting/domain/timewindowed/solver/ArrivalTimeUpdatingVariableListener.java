afde42035ef998e1cece489cd842ddeab43736a9
Ivan Reyes <ivanreyesconde@gmail.com>
2015-10-28 15:48:07 -0600

    Se modifica ArrivalTimeUpdatingVariableListener para tomar un dia de llegada

--- optaplanner-examples/src/main/java/org/optaplanner/examples/vehiclerouting/domain/timewindowed/solver/ArrivalTimeUpdatingVariableListener.java
+++ optaplanner-examples/src/main/java/org/optaplanner/examples/vehiclerouting/domain/timewindowed/solver/ArrivalTimeUpdatingVariableListener.java
@@ -23,9 +23,12 @@ import org.optaplanner.examples.vehiclerouting.domain.Customer;
 import org.optaplanner.examples.vehiclerouting.domain.Standstill;
 import org.optaplanner.examples.vehiclerouting.domain.timewindowed.TimeWindowedCustomer;
 
+import java.util.logging.Logger;
+
+
 // TODO When this class is added only for TimeWindowedCustomer, use TimeWindowedCustomer instead of Customer
 public class ArrivalTimeUpdatingVariableListener implements VariableListener<Customer> {
-
+  private static Logger LOGGER = Logger.getLogger("InfoLogging");
     public void beforeEntityAdded(ScoreDirector scoreDirector, Customer customer) {
         // Do nothing
     }
@@ -59,6 +62,7 @@ public class ArrivalTimeUpdatingVariableListener implements VariableListener<Cus
         Long departureTime = (previousStandstill instanceof TimeWindowedCustomer)
                 ? ((TimeWindowedCustomer) previousStandstill).getDepartureTime() : null;
         TimeWindowedCustomer shadowCustomer = sourceCustomer;
+
         Long arrivalTime = calculateArrivalTime(shadowCustomer, departureTime);
         while (shadowCustomer != null && ObjectUtils.notEqual(shadowCustomer.getArrivalTime(), arrivalTime)) {
             scoreDirector.beforeVariableChanged(shadowCustomer, "arrivalTime");
@@ -71,14 +75,47 @@ public class ArrivalTimeUpdatingVariableListener implements VariableListener<Cus
     }
 
     private Long calculateArrivalTime(TimeWindowedCustomer customer, Long previousDepartureTime) {
-        if (customer == null) {
+
+//    	int range_s = 0;
+//    	int range_e = 0;
+    	
+//    	long fecha_inicio_rango = day_s[range_s] + customer.getReadyTime();
+//    	long fecha_fin_rango = day_s[range_e] + customer.getReadyTime();
+
+
+    	long[] day_s = new long[]{0, 1440000, 2880000, 4320000, 5760000, 7200000, 8640000, 12960000, 14400000, 10080000, 11520000, 15840000};
+    	long[] day_e = new long[]{1439999, 2879999, 4319999, 5759999, 7199999, 8639999, 10079999, 11519999, 12959999, 14399999, 15839999, 17279999};
+
+    	int dia_llegada = 0;// customer.getPreviousStandstill().dia_llegada()
+    	long llegada = 0;
+    	
+    	if (customer == null) {
             return null;
         }
         if (previousDepartureTime == null) {
             // PreviousStandstill is the Vehicle, so we leave from the Depot at the best suitable time
-            return Math.max(customer.getReadyTime(), customer.getDistanceFromPreviousStandstill());
+
+
+    		//customer.setdia_llegada(0)
+        	long a = customer.getReadyTime();
+            long b = customer.getDistanceFromPreviousStandstill();
+        	llegada = Math.max(a, b);
         }
-        return previousDepartureTime + customer.getDistanceFromPreviousStandstill();
+        else
+        {
+        llegada = previousDepartureTime + customer.getDistanceFromPreviousStandstill();
+        }
+
+        if (llegada > day_s[dia_llegada] + customer.getDueTime()) {
+//customer.setdia_llegada(dia_llegada + 1)
+        	return day_s[dia_llegada + 1] + customer.getReadyTime();
+
+        }
+        else {
+    		//customer.setdia_llegada(dia_llegada)		
+        	return day_s[dia_llegada] + llegada;
+
+        }
     }
 
 }
